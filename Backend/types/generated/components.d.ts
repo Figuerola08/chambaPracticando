2eba867bf5d43975c0aa8f28c2463493b7d64f7b
Figuerola08 <diegofiguerola@me.com>
2024-01-23 19:08:21 -0500

    botón

--- Backend/types/generated/components.d.ts
+++ Backend/types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'elements.button-link': ElementsButtonLink;
+    }
+  }
 }
